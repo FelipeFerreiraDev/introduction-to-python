cf823bec6793d733230b83050d6a0ee1141788c9
Felipe Henrique C. Ferreira <48157305+FelipeFerreiraDev@users.noreply.github.com>
2021-04-30 19:53:06 -0300

    Add files via upload

---
+++ capitulo_1_conceitos_basicos.py
@@ -0,0 +1,34 @@
+#Printar na tela
+print('Hello world')
+
+#Tipos de váriaveis
+
+variavel_tipo_string = 'Conjunto de caracteres alfanumericos'
+
+
+variavel_tipo_int = 12	#número inteiro
+
+variavel_tipo_float = 15.3 	# número com casas decimais
+
+variavel_tipo_bool = True	#Booleano
+
+variavel_tipo_lista = [1, 2, 'Paulo', 'Maria', True]
+
+
+variavel_tipo_dicionario = {'nome':'Felipe', 'idade':31}
+
+variavel_tipo_tupla = ('Felipe', 'Ana', 3)
+
+variavel_tipo_comentario = """Um comentário atribuido a uma variavel deixa de ser apenas um comentário, vira frase!!!"""
+
+nome, idade, sexo, altura = 'Maria', 32, 'F', 1.89
+
+#função input
+nome = input('Digite seu nome: ')
+print('Olá: ', nome)
+
+#print usando mascara de substituição
+print('Seja bem vindo(a) {}!!!'.format(nome))
+
+#print avançado --> usando de f' strings
+print(f'Seja bem vindo (a) {nome}, :)')
